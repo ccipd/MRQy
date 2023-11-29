82b926024651632c5d070e876586aab91ccfb1f1
MarcSag <m.saghiah@gmail.com>
2023-11-29 17:24:27 +0100

    Added comments to QCF.py

--- src/mrqy/QCF.py
+++ src/mrqy/QCF.py
@@ -31,16 +31,24 @@ warnings.filterwarnings("ignore")
 class BaseVolume_dicom(dict):
 
     def __init__(self, fname_outdir, v, ol,folder_foregrounds, sample_size, ch_flag):
+        # Initialize the dictionary
         dict.__init__(self)
 
+        # Initialize attributes within the dictionary
         self["warnings"] = [] 
         self["output"] = []
+        # Add patient information to the output list
         self.addToPrintList("Patient", v[1]['ID'], v, ol, 170)
+        # Add the directory content to the output list
         self["outdir"] = fname_outdir
         self.addToPrintList("Name of Images", os.listdir(fname_outdir + os.sep + v[1]['ID']), v, ol, 100)
+        # Add specific data attributes to the output list
         for i,j in enumerate(v[1]):
             if i != 0:
                 self.addToPrintList(j, v[1][j], v, ol, i)
+        
+        # Set dictionary keys with corresponding values for various calculations
+        
         # self.addToPrintList("MFR", v[1]['Manufacturer'], v, ol, 1)
         # self.addToPrintList("MFS", v[1]['MFS'], v, ol, 2)
         # self.addToPrintList("VRX", v[1]['VR_x'], v, ol, 3)
@@ -69,8 +77,10 @@ class BaseVolume_dicom(dict):
         self.addToPrintList("FBER", vol(v, sample_size, "FBER",folder_foregrounds, ch_flag), v, ol, 25)
         
     def addToPrintList(self, name, val, v, ol, il):
+        # Add a new key-value pair to the dictionary
         self[name] = val
         self["output"].append(name)
+        # Display information about the patient's metrics
         if name != 'Name of Images' and il != 170:
             print('%s-%s. The %s of the patient with the name of <%s> is %s' % (ol,il,name, v[1]['ID'], val))
 
@@ -78,10 +88,14 @@ class BaseVolume_dicom(dict):
 class BaseVolume_nondicom(dict):
 
     def __init__(self, fname_outdir, v, ol, sample_size, ch_flag):
+        # Initialize the dictionary
         dict.__init__(self)
 
+        # Initialize attributes within the dictionary
         self["warnings"] = [] 
         self["output"] = []
+        
+        # Add patient information to the output list
         self.addToPrintList("Patient", v[1], v, ol, 170)
         self["outdir"] = fname_outdir
         self.addToPrintList("Name of Images", os.listdir(fname_outdir + os.sep + v[1]), v, ol, 100)
@@ -109,8 +123,10 @@ class BaseVolume_nondicom(dict):
         self.addToPrintList("FBER", vol(v, sample_size, "FBER",fname_outdir, ch_flag), v, ol, 21)
         
     def addToPrintList(self, name, val, v, ol, il):
+        # Add a new key-value pair to the dictionary
         self[name] = val
         self["output"].append(name)
+        # Display information about the patient's metrics
         if name != 'Name of Images' and il != 170:
             print('%s-%s. The %s of the patient with the name of <%s> is %s' % (ol,il,name, v[1], val))
 
@@ -118,11 +134,17 @@ class BaseVolume_nondicom(dict):
 class BaseVolume_mat(dict):
 
     def __init__(self, fname_outdir, v, ol,folder_foregrounds, sample_size):
+        # Initialize the dictionary
         dict.__init__(self)
 
+        # Initialize attributes within the dictionary
         self["warnings"] = [] 
         self["output"] = []
+        
+        # Add patient information to the output list
         self.addToPrintList("Patient", v[1]['ID'], v, ol, 170)
+        
+         # Add directory content to the output list
         self["outdir"] = fname_outdir
         self.addToPrintList("Name of Images", os.listdir(fname_outdir + os.sep + v[1]['ID']), v, ol, 100)
         self.addToPrintList("ROWS", np.shape(v[0])[0], v, ol, 1)
@@ -146,18 +168,21 @@ class BaseVolume_mat(dict):
         self.addToPrintList("FBER", vol(v, sample_size, "FBER",folder_foregrounds), v, ol, 18)
         
     def addToPrintList(self, name, val, v, ol, il):
+        # Add a new key-value pair to the dictionary
         self[name] = val
         self["output"].append(name)
+        # Display information about the patient's metrics
         if name != 'Name of Images' and il != 170:
             print('%s-%s. The %s of the patient with the name of <%s> is %s' % (ol,il,name, v[1]['ID'], val))
 
 def vol(v, sample_size, kk, outi_folder, ch_flag):
+    # Dictionary mapping each metric's name to its corresponding function
     switcher={
             'Mean': mean,
             'Range': rang,
             'Variance': variance, 
             'CV': percent_coefficient_variation,
-            'CPP': contrast_per_pixle,
+            'CPP': contrast_per_pixel,
             'PSNR': fpsnr,
             'SNR1': snr1,
             'SNR2': snr2,
@@ -169,43 +194,55 @@ def vol(v, sample_size, kk, outi_folder, ch_flag):
             'EFC': efc,
             'FBER': fber,
             }
+    # Retrieve the appropriate function based on the provided metric name
     func=switcher.get(kk)
     M = []
+    # Iterate through the volume data
     for i in range(1, len(v[0]), sample_size):
         I = v[0][i]
 #        I = I - np.min(I)  # for CT 
+        # Calculate foreground and background intensities
         F, B, c, f, b = foreground(I,outi_folder,v,i)
+        # Check if the standard deviation of foreground is zero, skip computing measures
         if np.std(F) == 0:  # whole zero slice, no measure computing
             continue
+        # Calculate the measure using the corresponding function
         measure = func(F, B, c, f, b)
+        # If the measure is NaN or infinite, skip and continue
         if np.isnan(measure) or np.isinf(measure):
             continue
             # measure = 0
-        # To do (add something)
+        # Append the calculated measure
         M.append(measure)
+    # Return the mean of all calculated measures
     return np.mean(M)
        
 
-
 def foreground(img,save_folder,v,inumber):
     try:
+        # Perform histogram equalization on the image
         h = ex.equalize_hist(img[:,:])*255
+        # Binary thresholding using Otsu's method on the original and histogram-equalized images
         oi = np.zeros_like(img, dtype=np.uint16)
         oi[(img > threshold_otsu(img)) == True] = 1
         oh = np.zeros_like(img, dtype=np.uint16)
         oh[(h > threshold_otsu(h)) == True] = 1
+        # Compute weights for the images based on the thresholding results
         nm = img.shape[0] * img.shape[1]
         w1 = np.sum(oi)/(nm)
         w2 = np.sum(oh)/(nm)
+        # Compute a new image using the calculated weights
         ots = np.zeros_like(img, dtype=np.uint16)
         new =( w1 * img) + (w2 * h)
         ots[(new > threshold_otsu(new)) == True] = 1 
+        # Obtain convex hull of the thresholded image
         conv_hull = convex_hull_image(ots)
-        conv_hull = convex_hull_image(ots)
+        # Calculate the foreground and background images based on the convex hull
         ch = np.multiply(conv_hull, 1)
         fore_image = ch * img
         back_image = (1 - ch) * img
     except Exception: 
+        # If an exception occurs, return default values
         fore_image = img.copy()
         back_image = np.zeros_like(img, dtype=np.uint16)
         conv_hull = np.zeros_like(img, dtype=np.uint16)
@@ -215,14 +252,13 @@ def foreground(img,save_folder,v,inumber):
     return fore_image, back_image, conv_hull, img[conv_hull], img[conv_hull==False]
 
 
-
 # def vol(v, sample_size, kk,outi_folder, ch_flag):
 #     switcher={
 #             'Mean': mean,
 #             'Range': rang,
 #             'Variance': variance, 
 #             'CV': percent_coefficient_variation,
-#             'CPP': contrast_per_pixle,
+#             'CPP': contrast_per_pixel,
 #             'PSNR': fpsnr,
 #             'SNR1': snr1,
 #             'SNR2': snr2,
@@ -283,33 +319,44 @@ def foreground(img,save_folder,v,inumber):
 #         plt.imsave(save_folder + os.sep + v[1]['ID'] +'(%d).png' % int(inumber+1), scipy.ndimage.rotate(ch,0), cmap = cm.Greys_r)
 #     return fore_image, back_image, conv_hull, img[conv_hull], img[conv_hull==False]
 
+
+# Computing the different metrics
+
 def mean(F, B, c, f, b):
     return np.nanmean(f)
+
 
 def rang(F, B, c, f, b):
     return np.ptp(f)
 
+
 def variance(F, B, c, f, b):
     return np.nanvar(f)
+
 
 def percent_coefficient_variation(F, B, c, f, b):
     return (np.nanstd(f)/np.nanmean(f))*100
 
-def contrast_per_pixle(F, B, c, f, b):
+
+def contrast_per_pixel(F, B, c, f, b):
     filt = np.array([[ -1/8, -1/8, -1/8],[-1/8, 1, -1/8],[ -1/8, -1/8,  -1/8]])
     I_hat = conv2(F, filt, mode='same')
     return np.nanmean(I_hat)
+
 
 def psnr(img1, img2):
     mse = np.square(np.subtract(img1, img2)).mean()
     return 20 * np.log10(np.nanmax(img1) / np.sqrt(mse))
 
+
 def fpsnr(F, B, c, f, b):
     I_hat = median(F/np.max(F), square(5))
     return psnr(F, I_hat)
 
+
 def snr1(F, B, c, f, b):
     return np.nanstd(f) / np.nanstd(b)
+
 
 def patch(img, patch_size):
     h = int(np.floor(patch_size / 2))
@@ -320,30 +367,37 @@ def patch(img, patch_size):
     b = b[0]
     return U[a:a+2*h+1,b:b+2*h+1]
 
+
 def snr2(F, B, c, f, b):
     fore_patch = patch(F, 5)
     return np.nanmean(fore_patch) / np.nanstd(b)
 
+
 def snr3(F, B, c, f, b):
     fore_patch = patch(F, 5)
     return np.nanmean(fore_patch)/np.nanstd(fore_patch - np.nanmean(fore_patch))
+
 
 def snr4(F, B, c, f, b):
     fore_patch = patch(F, 5)
     back_patch = patch(B, 5)
     return np.nanmean(fore_patch) / np.nanstd(back_patch)
 
+
 def cnr(F, B, c, f, b):
     fore_patch = patch(F, 5)
     back_patch = patch(B, 5)
     return np.nanmean(fore_patch-back_patch) / np.nanstd(back_patch)
 
+
 def cvp(F, B, c, f, b):
     fore_patch = patch(F, 5)
     return np.nanstd(fore_patch) / np.nanmean(fore_patch)
 
+
 def cjv(F, B, c, f, b):
     return (np.nanstd(f) + np.nanstd(b)) / abs(np.nanmean(f) - np.nanmean(b))
+
 
 def efc(F, B, c, f, b):
     n_vox = F.shape[0] * F.shape[1]
@@ -353,6 +407,7 @@ def efc(F, B, c, f, b):
     b_max = np.sqrt(abs(cc))
     return float((1.0 / abs(efc_max)) * np.sum(
         (F / b_max) * np.log((F + 1e16) / b_max)))
+
 
 def fber(F, B, c, f, b):
     fg_mu = np.nanmedian(np.abs(f) ** 2)
