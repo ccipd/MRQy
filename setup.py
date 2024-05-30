24f82df8cfcfdb97e68c5208efb79db29c1ef657
Mohsen Hariri <mhariri68@gmail.com>
2024-05-29 22:36:58 -0400

    py setup

---
+++ setup.py
@@ -0,0 +1,7 @@
+"""
+For the time being, pip still might require a setup.py file to support editable installs.
+There is no need to use setup.py in future versions of pip.
+"""
+from setuptools import setup
+
+setup()
